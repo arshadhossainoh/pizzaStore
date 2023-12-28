480b7d6d75a2f10240c985cbba237e3b6a8fadd6
arshadhossainoh <arshadhossain.oh@gmail.com>
2023-12-28 13:22:36 -0500

    worked on the nav buttons and designed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact Component={Home} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Logo from "../images/pizzaLogo.png";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faBars } from "@fortawesome/free-solid-svg-icons";
+function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+  return (
+    <div className="navbar">
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img src={Logo} />
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+
+      <div className="rightSide">
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <button>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
