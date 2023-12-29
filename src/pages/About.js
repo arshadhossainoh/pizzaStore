3d71ec220cd4885c4ebb7a21318b83eb0bcc4a49
arshadhossainoh <arshadhossain.oh@gmail.com>
2023-12-28 20:13:30 -0500

    fixed some typos

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,14 +12,15 @@ function About() {
         <h1>ABOUT US</h1>
         <p>
           We are 15 years old Pizza Store. We started as a small business and
-          grew over time. We pride ourselves in our servies and strive to meet
+          grew over time. We pride ourselves in our services and strive to meet
           our customer demands each and every day. We ensure all the ingredients
           in our pizza are fresh and top-notch quality. We greet people with
-          smile and love to see us being part of their jouney. That's an amazing
-          thing. Our customers love to come back to our store and let us know
-          how good our pizzas are. we are truly grateful for that..As we are
-          growing we now focus on varities on our items so that people can taste
-          something new. wanna give it a go, say what ?
+          smile and love to see us being a small part of their jouney. That's an
+          amazing thing, isnt it ? Our customers love to come back to our store
+          and let us know how good our pizzas are. we are truly grateful for
+          that..As we are growing we now focus on varities on our items so that
+          people can taste something new. May be one day you stop by and give it
+          a go, will ya ?
         </p>
       </div>
     </div>
